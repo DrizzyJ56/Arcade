b8759186ea1f83ee0e4465ca81a7ceda82234faf
DrizzyJ56 <blevins.j921@gmail.com>
2022-10-04 15:42:04 -0400

    End of day/Why Win Function not working?

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ function showPlayers(){
 }
 
 
-
 const gameState = {
     players: ['X', 'O'],
     board: [
@@ -24,12 +23,26 @@ const gameState = {
       [null, null, null]
     ]
   }
+function winGame(){
+  //Rows Below
+  if(gameState.board[0][0] === gameState.board[0][1] && gameState.board[0][0] === gameState.board[0][2] && gameState.board[0][0] !== null){
+      console.log('You Win')
+    }
+    if(gameState.board[1][0] === gameState.board[1][1] && gameState.board[1][0] === gameState.board[1][2] && gameState.board[1][0] !== null){
+      console.log('You Win')
+    }
+    if(gameState.board[2][0] === gameState.board[2][1] && gameState.board[2][0] === gameState.board[2][2] && gameState.board[2][0] !== null){
+      console.log('You Win')
+    }
+    //Columns Below
+
+    //Diagonal Below
+}
   playerOne = gameState.players[0]
   playerTwo = gameState.players[1]
 
   grabTable.addEventListener('click', function (clickEvent){
-    tdGrab = document.getElementsByTagName('td')
-    console.log(clickEvent.target)
+    console.log(gameState.board)
     if(turnCounter %2 === 0 && clickEvent.target.innerText === ''){
       clickEvent.target.innerText = playerOne
       turnCounter++
@@ -39,4 +52,15 @@ const gameState = {
     } else{
       alert('Someone has already played here')
     }
+    gameState.board[0][0] = document.getElementById('spotOne').innerText
+    gameState.board[0][1] = document.getElementById('spotTwo').innerText
+    gameState.board[0][2] = document.getElementById('spotThree').innerText
+    gameState.board[1][0] = document.getElementById('spotFour').innerText
+    gameState.board[1][1] = document.getElementById('spotFive').innerText
+    gameState.board[1][2] = document.getElementById('spotSix').innerText
+    gameState.board[2][0] = document.getElementById('spotSeven').innerText
+    gameState.board[2][1] = document.getElementById('spotEight').innerText
+    gameState.board[2][2] = document.getElementById('spotNine').innerText
+    winGame()
   })
+  
